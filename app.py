963bd4c4e0b005a1f6bc68a74510c5821a4871a6
Chris Brooks <chris@chrisbrooks.org>
2025-04-21 10:44:44 -0400

    Working solution

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask, request
+from flask import Flask, abort, request
 
 
 app = Flask(__name__)
 
 
-@app.route("/some_route_here", methods=["GET"])
-def method_name():
-    return {}
+@app.route("/api/calcs/<number>", methods=["GET"])
+def calculate(number):
+    try:
+        number = int(number)
+    except ValueError:
+        abort(400, description="The provided value must be an integer.")
+    
+    dec = number - 1
+    hexadecimal = hex(number)
+    return {'dec': dec, 'hex': hexadecimal}
